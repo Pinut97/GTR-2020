370d2fcb7a14fcbb331a62129f8a58c0d7732fca
Albarranco <alvar.alonso01@estudiant.upf.edu>
2020-06-27 20:10:04 +0200

    ambient light inicialitzada

--- src/scene.cpp
+++ src/scene.cpp
@@ -15,6 +15,7 @@ Scene::Scene()
 	ambientLight = Vector3(0.1f, 0.1f, 0.1f);
 	gizmoEntity = nullptr;
 	ambient_occlusion = true;
+	ambient_light = true;
 }
 
 void Scene::render(Camera* camera, GTR::Renderer* renderer) {
